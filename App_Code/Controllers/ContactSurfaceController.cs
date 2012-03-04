0a1ede4c07981f1d2e1d3cd1f8c501c920ac054d
asapostolov <aapostolov87@abv.bg>
2012-03-04 23:25:29 +0200

    Added base functionality for Contact form

---
+++ App_Code/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Cms.Web.Surface;
+using System.Web.Mvc;
+
+/// <summary>
+/// Surfase controller for Contact form
+/// </summary>
+public class ContactSurfaceController:SurfaceController
+{
+    [HttpPost]
+    public ActionResult SendContactInfo(
+        [Bind(Prefix = "MyTestForm")]
+            ContactModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return CurrentUmbracoPage();
+        }
+
+
+        return new JavaScriptResult() { Script="alert('Your message is sent! We will contact you for more information.')"};
+        //return new RedirectResult("/");
+    }
+}
---
+++ App_Code/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// The model for Contact form
+/// </summary>
+public class ContactModel
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    public string Message { get; set; }
+}
