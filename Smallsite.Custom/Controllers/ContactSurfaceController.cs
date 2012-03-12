b3c6e90ebfdfa8a8f8a5ba252d27bddc43d962fc
Apostol Apostolov <aapostolov87@abv.bg>
2012-03-12 12:21:32 +0200

    Refactoring of Project Structure.
    Created Smallsite.Custom project for the Custom Controller that inherits ServiceController.

---
+++ Smallsite.Custom/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Cms.Web.Surface;
+using System.Web.Mvc;
+
+namespace Smallsite.Custom
+{
+    /// <summary>
+    /// Surfase controller for Contact form
+    /// </summary>
+    public class ContactSurfaceController : SurfaceController
+    {
+        [ChildActionOnly]
+        public PartialViewResult ContactForm()
+        {
+            var model = new ContactModel();
+            return PartialView(model);
+        }
+
+        [HttpPost]
+        public ActionResult SendContactInfo(
+            [Bind(Prefix = "contactForm")]
+            ContactModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+
+
+            //return new JavaScriptResult() { Script="alert('Your message is sent! We will contact you for more information.')"};
+            //return new RedirectResult("/");
+            return new JsonResult() { Data = new { Name = "Sucess!" } };
+        }
+    }
+}
---
+++ Smallsite.Custom/Models/ContactModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using DataAnnotationsExtensions;
+
+namespace Smallsite.Custom
+{
+    /// <summary>
+    /// The model for Contact form
+    /// </summary>
+    public class ContactModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [Email]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
